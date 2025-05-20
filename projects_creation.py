f92751b073420194ba41da549aff95483586fa8d
Yozlen Aliev <yozlenaliev@gmail.com>
2025-05-20 11:36:13 +0300

    Added file projects_creation.py.

---
+++ projects_creation.py
@@ -0,0 +1,6 @@
+name_of_architect = input()
+number_of_projects_to_complete = int(input())
+total_time_needed_to_complete_all_projects = number_of_projects_to_complete * 3
+
+print(f"The architect {name_of_architect} will need {total_time_needed_to_complete_all_projects} hours"
+    f" to complete {number_of_projects_to_complete} project/s.")
