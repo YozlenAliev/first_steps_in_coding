310979e3afaeb2b8a71922695d691035e6c90a0e
Yozlen Aliev <yozlenaliev@gmail.com>
2025-05-20 11:38:26 +0300

    Added file pet_shop.py.

---
+++ pet_shop.py
@@ -0,0 +1,7 @@
+dog_package_count = int(input())
+cat_package_count = int(input())
+dog_price = dog_package_count * 2.50
+cat_price = cat_package_count * 4
+total = cat_price + dog_price
+
+print(f'{total} lv.')
