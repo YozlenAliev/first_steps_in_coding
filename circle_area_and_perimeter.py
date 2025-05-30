106b33d5d148bfbfdbe8169c496ac811c7f560e9
Yozlen Aliev <yozlenaliev@gmail.com>
2025-05-30 18:19:00 +0300

    Added file circle_area_and_perimeter.py

---
+++ circle_area_and_perimeter.py
@@ -0,0 +1,9 @@
+from math import pi
+
+r = float(input())
+
+calculated_area = pi * (r * r)
+calculated_parameter = 2 * pi * r
+
+print("%.2f" % calculated_area)
+print("%.2f" % calculated_parameter)
